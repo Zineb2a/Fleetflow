86a309402e93b7539f7ed1c1d1a579177772451a
bhavya02468 <108878066+bhavya02468@users.noreply.github.com>
2024-01-20 12:08:52 -0500

    Add files via upload

---
+++ sign.js
@@ -0,0 +1,12 @@
+$("#mycart").click(function () {
+    location.href = "mycart.html";
+})
+$("#home").click(function () {
+    location.href = "home.html";
+})
+$("#logout").click(function () {
+    location.href = "login.html";
+})
+
+
+
