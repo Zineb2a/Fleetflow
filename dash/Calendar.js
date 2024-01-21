f93494489d6c5361429341edd7c483005cf0cfb3
Hawa-Afnane Said <hawa21244@gmail.com>
2024-01-21 01:06:08 -0500

    updated calendar to only show october+november 2022

--- dash/Calendar.js
+++ dash/Calendar.js
@@ -7,12 +7,16 @@ $(document).ready(function() {
             center: 'title',
             right: 'month,agendaWeek,agendaDay,listWeek' // Include listWeek for weekly list view
         },
+        validRange: {
+            start: '2022-10-01',
+            end: '2022-11-30'
+        },
         events: [
             {
-              
+                // Your events data here
             },
             {
-                
+                // Your events data here
             }
             // Add more events with date and time
         ],
